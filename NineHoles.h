8d4dfbbdbcabf4273be7b9ced29571e8c8fed8b9
MnoZombie956 <34872596+MnoZombie956@users.noreply.github.com>
2021-04-26 21:33:23 -0300

    updateStatusBar method added

---
+++ NineHoles.h
@@ -0,0 +1,66 @@
+#ifndef NINEHOLES_H
+#define NINEHOLES_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class NineHoles; }
+QT_END_NAMESPACE
+
+class Hole;
+
+class NineHoles : public QMainWindow {
+    Q_OBJECT
+
+public:
+    enum Player {
+        RedPlayer,
+        BluePlayer
+    };
+    Q_ENUM(Player)
+
+    enum Phase {
+        DropPhase,
+        MovePhase
+    };
+    Q_ENUM(Phase)
+
+    NineHoles(QWidget *parent = nullptr);
+    virtual ~NineHoles();
+
+    Hole* holeAt(int row, int col) const;
+
+signals:
+    void gameOver(Player player);
+
+private:
+    Ui::NineHoles *ui;
+    Hole* m_holes[9];
+    Player m_player;
+    Phase m_phase;
+    int m_dropCount;
+    Hole* m_selected;
+
+    void drop(Hole* hole);
+    void move(Hole* hole);
+
+    void switchPlayer();
+    void clearSelectable();
+    QList<Hole*> findSelectable(Hole* hole);
+
+    bool checkRow(Player player, int col);
+    bool checkCol(Player player, int row);
+    bool checkDiagonals(Player player);
+    bool isGameOver(Hole* hole);
+
+private slots:
+    void play(int id);
+    void reset();
+    void showAbout();
+    void showAuthor();
+    void showGameOver(Player player);
+    void updateStatusBar();
+
+};
+
+#endif // NINEHOLES_H
